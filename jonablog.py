a0a1854a1b651aa6703bd4c931db7d8d30c8bb50
ChocFloat <jonavellga@gmail.com>
2024-11-26 14:21:56 +0800

    Add files via upload

---
+++ jonablog.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+# Title of the Biography
+st.title("Biography About Me")
+
+# Introduction Section
+st.write("Hello, I am Jonavell Ga. I am a student at Surigao del Norte State University.")
+
+# Personal Information Section
+st.write("*Full Name*: Jonavell Ga")
+st.write("*Location*: Crossing,Kitcharao ADN")
+st.write("*Email*: jonavellga@gmail.com")
+st.write("*Occupation*: Student")
+st.write("*Educational Attainment*: Graduated in Elementary and High School")
